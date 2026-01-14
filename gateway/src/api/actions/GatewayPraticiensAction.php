5889924d6a2d4564f7ebd965c2a20d73e9b237c0
tuline-leveque <tuline.leveque@gmail.com>
2026-01-14 16:58:56 +0100

    update(gateway): error / return
    Modification type retour + gestion erreurs

--- gateway/src/api/actions/GatewayPraticiensAction.php
+++ gateway/src/api/actions/GatewayPraticiensAction.php
@@ -4,11 +4,13 @@ namespace gateway\api\actions;
 
 use Exception;
 use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ConnectException;
 use GuzzleHttp\Exception\ServerException;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Exception\HttpNotFoundException;
+use toubilib\core\exceptions\ConnexionException;
 
 class GatewayPraticiensAction {
     private ClientInterface $remote_praticien_service;
@@ -20,15 +22,59 @@ class GatewayPraticiensAction {
     /**
      * @throws \Exception
      */
-    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+    public function __invoke(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        array $args
+    ): ResponseInterface {
+
         $path = ltrim($request->getUri()->getPath(), '/');
+
         try {
-            return $this->remote_praticien_service->request(
+            $apiResponse = $this->remote_praticien_service->request(
                 $request->getMethod(),
                 $path
             );
-        } catch (ClientException $e) {
-            throw new HttpNotFoundException($request, $e);
+
+            $response->getBody()->write(
+                $apiResponse->getBody()->getContents()
+            );
+
+            foreach ($apiResponse->getHeaders() as $name => $values) {
+                $response = $response->withHeader($name, $values);
+            }
+
+            return $response->withStatus($apiResponse->getStatusCode());
+
+        } catch (ClientException | ServerException $e) {
+            $response->getBody()->write(
+                $e->getResponse()->getBody()->getContents()
+            );
+            return $response
+                ->withStatus($e->getResponse()->getStatusCode())
+                ->withHeader('Content-Type', 'application/json');
+
+        } catch (ConnectException | ConnexionException) {
+            $response->getBody()->write(json_encode([
+                'error' => 'AUTH_SERVICE_UNAVAILABLE',
+                'message' => 'Authentication service unreachable'
+            ]));
+
+            return $response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(502);
+
+        } catch (\Throwable $e) {
+            $response->getBody()->write(json_encode([
+                'error' => 'GATEWAY_INTERNAL_ERROR',
+                'message' => 'Unexpected gateway error'
+            ]));
+
+            return $response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(500);
         }
     }
+
+
 }
--- gateway/src/api/actions/GatewayRendezVousAction.php
+++ gateway/src/api/actions/GatewayRendezVousAction.php
@@ -4,11 +4,13 @@ namespace gateway\api\actions;
 
 use Exception;
 use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ConnectException;
 use GuzzleHttp\Exception\ServerException;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Exception\HttpNotFoundException;
+use toubilib\core\exceptions\ConnexionException;
 
 class GatewayRendezVousAction {
     private ClientInterface $remote_rendezVous_service;
@@ -20,15 +22,58 @@ class GatewayRendezVousAction {
     /**
      * @throws \Exception
      */
-    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+    public function __invoke(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        array $args
+    ): ResponseInterface {
+
         $path = ltrim($request->getUri()->getPath(), '/');
+
         try {
-            return $this->remote_rendezVous_service->request(
+            $apiResponse = $this->remote_rendezVous_service->request(
                 $request->getMethod(),
                 $path
             );
-        } catch (ClientException $e) {
-            throw new HttpNotFoundException($request, $e);
+
+            $response->getBody()->write(
+                $apiResponse->getBody()->getContents()
+            );
+
+            foreach ($apiResponse->getHeaders() as $name => $values) {
+                $response = $response->withHeader($name, $values);
+            }
+
+            return $response->withStatus($apiResponse->getStatusCode());
+
+        } catch (ClientException | ServerException $e) {
+            $response->getBody()->write(
+                $e->getResponse()->getBody()->getContents()
+            );
+            return $response
+                ->withStatus($e->getResponse()->getStatusCode())
+                ->withHeader('Content-Type', 'application/json');
+
+        } catch (ConnectException | ConnexionException) {
+            $response->getBody()->write(json_encode([
+                'error' => 'AUTH_SERVICE_UNAVAILABLE',
+                'message' => 'Authentication service unreachable'
+            ]));
+
+            return $response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(502);
+
+        } catch (\Throwable $e) {
+            $response->getBody()->write(json_encode([
+                'error' => 'GATEWAY_INTERNAL_ERROR',
+                'message' => 'Unexpected gateway error'
+            ]));
+
+            return $response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(500);
         }
     }
+
 }
