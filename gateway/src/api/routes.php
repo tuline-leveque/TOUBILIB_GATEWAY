38925b5674dd2e9e5f43bd30667d29485ca574e3
suki <88000sofien@gmail.com>
2026-01-21 15:29:13 +0100

    🚑️ gateway : create missing routes & fix params transfert
    Ajout des routes manquantes pour :
    - GET Patient RDV
    - GET RDV (avec un ID donner)
    - POST Create RDV
    - POST Create Praticien Indisponibilite
    - DELETE RDV
    - PATCH RDV
    
    Adaptation de l'action GatewayPraticiens pour qu'il passe le contenu du Body de la requete lors de la creation d'une nouvelle indisponibilite

--- gateway/src/api/routes.php
+++ gateway/src/api/routes.php
@@ -8,14 +8,33 @@ use Slim\App;
 
 return function( App $app): App {
 //    GET
+    // PRATICIENS
     $app->get('/praticiens', GatewayPraticiensAction::class);
     $app->get('/praticiens/{id}', GatewayPraticiensAction::class);
+
+    // RDV
     $app->get('/praticiens/{id}/rdvs', GatewayRendezVousAction::class);
+    $app->get('/patients/{id}/rdvs', GatewayRendezVousAction::class);
+    $app->get('/praticiens/{id}/rdvs/{id_rdv}', GatewayRendezVousAction::class);
 
 //    POST
+    // AUTH
     $app->post('/signin', GatewayAuthAction::class);
     $app->post('/register', GatewayAuthAction::class);
     $app->post('/refresh', GatewayAuthAction::class);
 
+    // PRATICIENS
+    $app->post("/praticiens/{id_prat}/indisponibilites", GatewayPraticiensAction::class);
+
+    // RDV
+    $app->post('/praticiens/{id}/rdvs', GatewayRendezVousAction::class);
+
+//    DELETE
+    // RDV
+    $app->delete('/praticiens/{id}/rdvs/{id_rdv}', GatewayRendezVousAction::class);
+
+//    PATCH
+    // RDV
+    $app->patch('/praticiens/{id}/rdvs/{id_rdv}', GatewayRendezVousAction::class);
     return $app;
 };
