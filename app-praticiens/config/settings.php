47972f1d02493d26feb21cb933d00d8df758d293
tuline-leveque <tuline.leveque@gmail.com>
2026-01-21 14:32:59 +0100

    update(settings): praticiens/rdv
    Modification des settings de l'app-praticiens et l'app-rdv pour n'avoir que cequi les concerne.

--- app-praticiens/config/settings.php
+++ app-praticiens/config/settings.php
@@ -17,28 +17,12 @@ return [
         return new \PDO($dsn, $user, $password, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
     },
 
-    'rdv.pdo' => function (ContainerInterface $c) {
-        $config = parse_ini_file($c->get('db.config'));
-        $dsn = "{$config['rdv.driver']}:host={$config['rdv.host']};dbname={$config['rdv.database']}";
-        $user = $config['rdv.username'];
-        $password = $config['rdv.password'];
-        return new \PDO($dsn, $user, $password, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
-    },
-
     'pat.pdo' => function (ContainerInterface $c) {
         $config = parse_ini_file($c->get('db.config'));
         $dsn = "{$config['pat.driver']}:host={$config['pat.host']};dbname={$config['pat.database']}";
         $user = $config['pat.username'];
         $password = $config['pat.password'];
         return new \PDO($dsn, $user, $password, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
-    },
-
-    'auth.pdo' => function (ContainerInterface $c) {
-        $config = parse_ini_file($c->get('db.config'));
-        $dsn = "{$config['auth.driver']}:host={$config['auth.host']};dbname={$config['auth.database']}";
-        $user = $config['auth.username'];
-        $password = $config['auth.password'];
-        return new \PDO($dsn, $user, $password, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
-    },
+    }
 ];
 
