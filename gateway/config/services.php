68dd246261bb409aebea487a0570aa2e2605577d
suki <88000sofien@gmail.com>
2026-01-26 16:54:26 +0100

    🚑️⬆️ Fix dependancy : Correction erreur copier-coller
    GatewayPraticienAction etait utiliser a la place de GatewayRendezVousAction.php

--- gateway/config/services.php
+++ gateway/config/services.php
@@ -24,7 +24,7 @@ return [
         ]);
     },
     GatewayRendezVousAction::class => function (ContainerInterface $c) {
-        return new GatewayPraticiensAction($c->get('rdvs.guzzle.client'));
+        return new GatewayRendezVousAction($c->get('rdvs.guzzle.client'));
     },
     'auth.guzzle.client' => function (ContainerInterface $c) {
         return new Client([
